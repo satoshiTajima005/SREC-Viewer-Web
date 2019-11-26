ed97c225973bb5a8cd98dc288c67a4daff535cb3
satoshiTajima005 <satoshi.tajima@gmail.com>
2019-11-26 14:59:02 +0900

    debug

--- js/component.js
+++ js/component.js
@@ -376,7 +376,7 @@ Vue.component('AisTree', {
       <div class="columns">
         <div class="column">
           <ul>
-            <ais-tree-child v-for="(child, index) in target.children" :key="index" :item="child" :treeID="tree0"></ais-tree-child>
+            <ais-tree-child v-for="(child, index) in target.children" :key="index" :item="child" treeID="tree"></ais-tree-child>
           </ul>
         </div>
         <div class="column">
